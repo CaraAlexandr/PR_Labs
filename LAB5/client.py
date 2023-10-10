5a57c5719d9642466b22a92123674d31510f067e
CaraAlexandr <alexandru.cara@snt.md>
2023-10-10 08:47:07 +0300

    Lab 5 HW

--- LAB5/client.py
+++ LAB5/client.py
@@ -1,62 +1,119 @@
 import socket
 import threading
 import json
+import os
+import uuid
+import base64
+from datetime import datetime
+from colorama import init, Fore
+
+init(autoreset=True)
+
+CLIENT_MEDIA = f"CLIENT_MEDIA_{uuid.uuid4().hex}"
+if not os.path.exists(CLIENT_MEDIA):
+    os.mkdir(CLIENT_MEDIA)
 
 
 def format_message(msg_type, payload):
     return json.dumps({"type": msg_type, "payload": payload})
 
 
+def send_complete_message(client_socket, message):
+    message += '\n'  # append the delimiter
+    client_socket.sendall(message.encode('utf-8'))
+
+
 def display_message(message_data):
     msg_type = message_data.get("type")
     payload = message_data.get("payload", {})
+    timestamp = datetime.now().strftime("%H:%M:%S")
 
     if msg_type == "connect_ack":
-        print(f"Server: {payload.get('message')}")
+        print(Fore.GREEN + f"[{timestamp}] Server: {payload.get('message')}")
     elif msg_type == "exit_ack":
-        print(f"Server: {payload.get('message')}")
+        print(Fore.GREEN + f"[{timestamp}] Server: {payload.get('message')}")
     elif msg_type == "message":
         sender = payload.get("sender")
         text = payload.get("text")
-        print(f"{sender}: {text}")
+        print(Fore.BLUE + f"[{timestamp}] {sender}: {text}")
     elif msg_type == "notification":
-        print(f"Server Notification: {payload.get('message')}")
+        print(Fore.GREEN + f"[{timestamp}] Server Notification: {payload.get('message')}")
+    elif msg_type == "error":
+        print(Fore.RED + f"[{timestamp}] Error: {payload.get('message')}")
+    elif msg_type == "file":
+        file_name = message_data.get("payload", {}).get("file_name")
+        b64_encoded_content = message_data.get("payload", {}).get("content")
+        file_content = base64.b64decode(b64_encoded_content)
+        with open(os.path.join(CLIENT_MEDIA, file_name), 'wb') as f:
+            f.write(file_content)
+        print(Fore.YELLOW + f"[{timestamp}] File {file_name} downloaded successfully.")
+
+
+def receive_messages():
+    while True:
+        message = client_socket.recv(1024).decode('utf-8')
+        if not message:
+            break
+        try:
+            message_data = json.loads(message)
+            display_message(message_data)
+        except json.JSONDecodeError:
+            print("Received invalid JSON data.")
+
+
+def send_messages():
+    while True:
+        message_text = input(Fore.CYAN + "Enter a command (or 'exit' to quit): ")
+
+        if message_text.lower().startswith("upload:"):
+            file_path = message_text.split(":", 1)[1].strip()
+            if os.path.exists(file_path):
+                with open(file_path, 'rb') as f:
+                    file_content = f.read()
+                file_name = os.path.basename(file_path)
+                b64_encoded_content = base64.b64encode(file_content).decode('utf-8')
+                message = format_message("upload", {"file_name": file_name, "content": b64_encoded_content})
+                send_complete_message(client_socket, message)
+            else:
+                print(Fore.RED + f"File {file_path} doesn't exist.")
+
+        elif message_text.lower().startswith("download:"):
+            file_name = message_text.split(":", 1)[1].strip()
+            message = format_message("download", {"file_name": file_name})
+            send_complete_message(client_socket, message)
+
+        elif message_text.lower() == 'exit':
+            exit_message = format_message("exit", {"name": client_name, "room": room_name})
+            send_complete_message(client_socket, exit_message)
+            client_socket.close()
+            break
+
+        else:
+            message = format_message("message", {"sender": client_name, "room": room_name, "text": message_text})
+            send_complete_message(client_socket, message)
 
 
 def start_client():
+    print(Fore.YELLOW + "Welcome to the Chat Client!")
+    print(Fore.YELLOW + "Commands: ")
+    print(Fore.YELLOW + "1. To upload a file: upload: <path_to_file>")
+    print(Fore.YELLOW + "2. To download a file: download: <file_name>")
+    print(Fore.YELLOW + "3. To exit: exit")
+    print("-" * 50)
+
     HOST = '127.0.0.1'
     PORT = 12345
+    global client_socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((HOST, PORT))
     print(f"Connected to {HOST}:{PORT}")
 
+    global client_name
     client_name = input("Enter your name: ")
+    global room_name
     room_name = input("Enter room name: ")
     connect_message = format_message("connect", {"name": client_name, "room": room_name})
-    client_socket.send(connect_message.encode('utf-8'))
-
-    def receive_messages():
-        while True:
-            message = client_socket.recv(1024).decode('utf-8')
-            if not message:
-                break
-            try:
-                message_data = json.loads(message)
-                display_message(message_data)
-            except json.JSONDecodeError:
-                print("Received invalid JSON data.")
-
-    def send_messages():
-        while True:
-            message_text = input("Enter a message (or 'exit' to quit): ")
-            if message_text.lower() == 'exit':
-                exit_message = format_message("exit", {"name": client_name, "room": room_name})
-                client_socket.send(exit_message.encode('utf-8'))
-
-                client_socket.close()
-                break
-            message = format_message("message", {"sender": client_name, "room": room_name, "text": message_text})
-            client_socket.send(message.encode('utf-8'))
+    send_complete_message(client_socket, connect_message)
 
     receive_thread = threading.Thread(target=receive_messages)
     receive_thread.daemon = True
--- LAB5/server.py
+++ LAB5/server.py
@@ -1,13 +1,26 @@
 import socket
 import threading
 import json
+import os
+import base64
+
+SERVER_MEDIA = "SERVER_MEDIA"
+if not os.path.exists(SERVER_MEDIA):
+    os.mkdir(SERVER_MEDIA)
 
 clients = []
-
 
 def format_message(msg_type, payload):
     return json.dumps({"type": msg_type, "payload": payload})
 
+def receive_complete_message(client_socket):
+    chunks = []
+    while True:
+        chunk = client_socket.recv(1)  # receive byte by byte
+        if chunk == b'\n':  # message delimiter
+            break
+        chunks.append(chunk)
+    return b''.join(chunks).decode('utf-8')
 
 def handle_client(client_socket, client_address):
     global clients
@@ -17,7 +30,7 @@ def handle_client(client_socket, client_address):
 
     while True:
         try:
-            message_json = client_socket.recv(1024).decode('utf-8')
+            message_json = receive_complete_message(client_socket)
             if not message_json:
                 break
 
@@ -26,19 +39,19 @@ def handle_client(client_socket, client_address):
                 client_name = message_data["payload"]["name"]
                 client_room = message_data["payload"]["room"]
                 ack_message = format_message("connect_ack", {"message": "Connected to the room."})
-                client_socket.send(ack_message.encode('utf-8'))
+                client_socket.sendall((ack_message + '\n').encode('utf-8'))
                 notification = format_message("notification", {"message": f"{client_name} has joined the room."})
                 for client in clients:
-                    client.send(notification.encode('utf-8'))
+                    client.sendall((notification + '\n').encode('utf-8'))
 
-            if message_data["type"] == "exit":
+            elif message_data["type"] == "exit":
                 client_name = message_data["payload"]["name"]
                 client_room = message_data["payload"]["room"]
                 ack_message = format_message("exit_ack", {"message": "Disconnected to the room."})
-                client_socket.send(ack_message.encode('utf-8'))
+                client_socket.sendall((ack_message + '\n').encode('utf-8'))
                 notification = format_message("notification", {"message": f"{client_name} has left the room."})
                 for client in clients:
-                    client.send(notification.encode('utf-8'))
+                    client.sendall((notification + '\n').encode('utf-8'))
 
             elif message_data["type"] == "message":
                 broadcast_message = format_message("message", {
@@ -47,14 +60,38 @@ def handle_client(client_socket, client_address):
                     "text": message_data["payload"]["text"]
                 })
                 for client in clients:
-                    client.send(broadcast_message.encode('utf-8'))
+                    client.sendall((broadcast_message + '\n').encode('utf-8'))
+
+            elif message_data["type"] == "upload":
+                file_name = message_data["payload"]["file_name"]
+                b64_encoded_content = message_data["payload"]["content"]
+                file_content = base64.b64decode(b64_encoded_content)
+                file_path = os.path.join(SERVER_MEDIA, file_name)
+                with open(file_path, 'wb') as f:
+                    f.write(file_content)
+                print(f"File {file_name} saved to {file_path}")
+                notification = format_message("notification", {"message": f"User {client_name} uploaded the {file_name} file."})
+                for client in clients:
+                    client.sendall((notification + '\n').encode('utf-8'))
+
+            elif message_data["type"] == "download":
+                file_name = message_data["payload"]["file_name"]
+                file_path = os.path.join(SERVER_MEDIA, file_name)
+                if os.path.exists(file_path):
+                    with open(file_path, 'rb') as f:
+                        file_content = f.read()
+                    b64_encoded_content = base64.b64encode(file_content).decode('utf-8')
+                    send_data = format_message("file", {"file_name": file_name, "content": b64_encoded_content})
+                    client_socket.sendall((send_data + '\n').encode('utf-8'))
+                else:
+                    error_message = format_message("error", {"message": f"The {file_name} doesn't exist."})
+                    client_socket.sendall((error_message + '\n').encode('utf-8'))
 
         except json.JSONDecodeError:
             print(f"Received invalid JSON data from {client_address}. Continuing...")
 
     clients.remove(client_socket)
     client_socket.close()
-
 
 if __name__ == "__main__":
     HOST = '127.0.0.1'
