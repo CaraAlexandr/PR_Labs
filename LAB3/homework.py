a5da7b158fdc6a35c94376c933b2f52f3830f2ae
CaraAlexandr <alexandru.cara@snt.md>
2023-10-26 21:27:01 +0300

    Lab 3 homework and class work

--- LAB3/homework.py
+++ LAB3/homework.py
@@ -1,187 +1,83 @@
-import json
-import csv
 import requests
 from bs4 import BeautifulSoup
+import json
 
 
-# User-Agent Rotation: List of user agents to mimic different browsers.
-USER_AGENTS = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.3',
-    # Add more user agents if needed
-]
+def extract_info(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    dict_result = {}
 
-def fetch_web_content(product_url):
-    """Fetches the content of the given URL."""
-    headers = {
-        'User-Agent': USER_AGENTS[hash(product_url) % len(USER_AGENTS)]
-    }
-    try:
-        response = requests.get(product_url, headers=headers)
-        response.raise_for_status()
-        return BeautifulSoup(response.content, 'html.parser')
-    except requests.RequestException as e:
-        print(f"Error fetching URL {product_url}. Error: {e}")
-        return None
+    title = soup.find('h1', itemprop='name')
+    if title:
+        dict_result['Title'] = title.text
 
+    desc = soup.find('div', itemprop='description')
+    if desc:
+        dict_result['Description'] = desc.text
 
-def extract_owner_details(parsed_content):
-    """Extracts the owner details from the parsed content."""
-    owner_details = {}
-    owner_section = parsed_content.find('dl', {'class': 'adPage__aside__stats__owner'})
-    owner_details['Name'] = owner_section.find('a', {'class': 'adPage__aside__stats__owner__login buyer_experiment'}).text
-    owner_details['On website since'] = owner_section.find('span').text
-    last_update_section = owner_section.find_next('div')
-    owner_details['Last Update'] = last_update_section.text
-    ad_type_section = last_update_section.find_next('div')
-    owner_details['Ad type'] = ad_type_section.text
-    views_section = ad_type_section.find_next('div')
-    owner_details['Views'] = views_section.text
-    return owner_details
-
-
-def extract_product_info(parsed_content):
-    """Extracts product information from the parsed content."""
-    product_info = {}
-    description_section = parsed_content.find('div', {'class': 'adPage__content__description grid_18'})
-    product_info['Description'] = description_section.text
-    features_section = parsed_content.find('div', {'class': 'adPage__content__features'}).find_all('h2')
-    for feature in features_section:
-        feature_details = {}
-        ul_section = feature.find_next('ul')
-        if ul_section:
-            list_items = ul_section.find_all('li')
-            for item in list_items:
-                spans = item.find_all('span')
-                if len(spans) == 2:
-                    feature_details[spans[0].text] = spans[1].text
-                else:
-                    feature_details[spans[0].text] = 'None'
-        product_info[feature.text] = feature_details
-    return product_info
-
-
-def export_data(data_list, export_format, filename):
-    """Exports the scraped data to the specified format."""
-    if export_format == "JSON":
-        with open(f"{filename}.json", "w") as outfile:
-            outfile.write(json.dumps(data_list, indent=2, ensure_ascii=False))
-        print(f"Data saved to {filename}.json")
-    elif export_format == "TXT":
-        with open(f"{filename}.txt", "w") as outfile:
-            outfile.write(json.dumps(data_list, indent=2, ensure_ascii=False))
-        print(f"Data saved to {filename}.txt")
-    elif export_format == "CSV":
-        export_to_csv(f"{filename}.csv", data_list)
-        print(f"Data saved to {filename}.csv")
-    elif export_format == "CONSOLE":
-        print(json.dumps(data_list, indent=2, ensure_ascii=False))
-    else:
-        print("Invalid choice!")
-
-
-def export_to_csv(filename, data_list):
-    """Exports the scraped data to a CSV file."""
-    keys = data_list[0].keys()
-    with open(filename, 'w', newline='') as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(data_list)
-
-
-def read_urls_from_file(filename):
-    """Reads URLs from a given file."""
-    with open(filename, 'r') as file:
-        return [url.strip() for url in file.readlines()]
-
-
-def main():
-    """Main function to interact with the user and perform operations."""
-    all_data = []
-
-    while True:
-        print("\n--- Menu ---")
-        print("1. Scrape product details")
-        print("2. Scrape product details from a .txt file")
-        print("3. Export all scraped data")
-        print("4. Exit")
-        choice = input("Enter your choice: ")
-
-        if choice == "1":
-            # Scrape Multiple URLs: Allow user to input multiple URLs separated by commas.
-            target_urls = input("Enter the product URLs (comma-separated): ").split(',')
-            for url in target_urls:
-                parsed_content = fetch_web_content(url.strip())
-                if parsed_content:
-                    owner_details = extract_owner_details(parsed_content)
-                    product_info = extract_product_info(parsed_content)
-
-                    final_data = {
-                        'URL': url,
-                        'Owner Details': owner_details,
-                        'Product Info': product_info
-                    }
-                    all_data.append(final_data)
-                    print(f"Scraped data for {url}")
-
-        elif choice == "2":
-            # Scrape URLs from a .txt file
-            file_path = input("Enter the path to the .txt file containing the product URLs (without extension): ")+".txt"
-            try:
-                target_urls = read_urls_from_file(file_path)
-                for url in target_urls:
-                    parsed_content = fetch_web_content(url)
-                    if parsed_content:
-                        owner_details = extract_owner_details(parsed_content)
-                        product_info = extract_product_info(parsed_content)
-
-                        final_data = {
-                            'URL': url,
-                            'Owner Details': owner_details,
-                            'Product Info': product_info
-                        }
-                        all_data.append(final_data)
-                        print(f"Scraped data for {url}")
-            except FileNotFoundError:
-                print(f"Error: File '{file_path}' not found!")
-
-        elif choice == "3":
-            print("\n--- Export Options ---")
-            print("1. JSON")
-            print("2. TXT")
-            print("3. CSV")
-            print('4. Console')
-            export_choice = input("Enter your choice: ")
-
-
-
-            if export_choice == "1":
-                filename = input("Enter filename (without extension): ")
-                with open(f"{filename}.json", "w") as outfile:
-                    outfile.write(json.dumps(all_data, indent=2, ensure_ascii=False))
-                print(f"Data saved to {filename}.json")
-            elif export_choice == "2":
-                filename = input("Enter filename (without extension): ")
-                with open(f"{filename}.txt", "w") as outfile:
-                    outfile.write(json.dumps(all_data, indent=2, ensure_ascii=False))
-                print(f"Data saved to {filename}.txt")
-            elif export_choice == "3":
-                filename = input("Enter filename (without extension): ")
-                # Export to CSV: Save the scraped data into a CSV file.
-                export_to_csv(f"{filename}.csv", all_data)
-                print(f"Data saved to {filename}.csv")
-            elif export_choice == "4":
-                print(json.dumps(final_data, indent=2, ensure_ascii=False))  # Print the scraped data to console
-
-            else:
-                print("Invalid choice!")
-
-        elif choice == "4":
-            print("Exiting program. Goodbye!")
-            break
+    price = soup.find(
+        'span', class_='adPage__content__price-feature__prices__price__value')
+    currency = soup.find('span', itemprop='priceCurrency')
+    if price:
+        if price.text.find('negociabil') != -1:
+            dict_result['Price'] = price.text
         else:
-            print("Invalid choice! Please try again.")
+            dict_result['Price'] = price.text + ' ' + currency.get('content')
 
-if __name__ == '__main__':
-    main()
+    country = soup.find('meta', itemprop='addressCountry')
+    locality = soup.find('meta', itemprop='addressLocality')
+    if country and locality:
+        dict_result['Location'] = locality.get(
+            'content') + ', ' + country.get('content')
+
+    ad_info = {}
+    views = soup.find('div', class_='adPage__aside__stats__views')
+    if views:
+        ad_info['Views'] = views.text
+    date = soup.find('div', class_='adPage__aside__stats__date')
+    if date:
+        ad_info['Update Date'] = date.text
+    ad_type = soup.find('div', class_='adPage__aside__stats__type')
+    if ad_type:
+        ad_info['Ad Type'] = ad_type.text
+    owner_username = soup.find(
+        'a', class_='adPage__aside__stats__owner__login buyer_experiment  has-reviews')
+    if owner_username:
+        ad_info['Owner Username'] = owner_username.text
+    dict_result['Ad Info'] = ad_info
+
+    general_div = soup.find('div', class_='adPage__content__features__col')
+    general_dict = {}
+    li_elements = general_div.find_all('li')
+    for li in li_elements:
+        key_element = li.find('span', class_='adPage__content__features__key')
+        value_element = li.find(
+            'span', class_='adPage__content__features__value')
+        if key_element and value_element:
+            key = key_element.text.strip()
+            value = value_element.text.strip()
+            general_dict[key] = value
+    dict_result['General Info'] = general_dict
+
+    features_div = soup.find(
+        'div', class_='adPage__content__features__col grid_7 suffix_1')
+    features_dict = {}
+    li_elements = features_div.find_all('li')
+    for li in li_elements:
+        key_element = li.find('span', class_='adPage__content__features__key')
+        value_element = li.find(
+            'span', class_='adPage__content__features__value')
+        if key_element and value_element:
+            key = key_element.text.strip()
+            value = value_element.text.strip()
+            features_dict[key] = value
+    dict_result['Features'] = features_dict
+
+    print(json.dumps(dict_result, indent=4, ensure_ascii=False))
+
+    return dict_result
+
+
+URL = "https://999.md/ro/84307033"
+extract_info(URL)
--- LAB3/in_class.py
+++ LAB3/in_class.py
@@ -1,48 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-BASE_URL = 'https://999.md/ru/list/audio-video-photo/headphones'
+
+def recursive(maxIterations, currentIteration, URL_array, data):
+    URL = URL_array[currentIteration]
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    # Use a set to store the links that have been seen
+    seen_links = set(data)  # Initialize it with the links already in data
+
+    for link in soup.find_all('a', href=True, class_='js-item-ad'):
+        link = str(link.get('href'))
+        if link[1] != 'b':
+            url_to_append = 'https://999.md/' + link
+            if url_to_append not in seen_links:
+                seen_links.add(url_to_append)
+                data.append(url_to_append)
+
+    pages = soup.select('nav.paginator > ul > li > a')
+    for page in pages:
+        link = str('https://999.md' + page['href'])
+        if link not in URL_array:
+            URL_array.append(link)
+
+    if currentIteration == maxIterations or currentIteration >= len(URL_array) - 1:
+        print(data)
+        print(len(data))
+        return data
+    else:
+        recursive(maxIterations, currentIteration + 1, URL_array, data)
 
 
-def fetch_html(url):
-    response = requests.get(url)
-    return response.text
-
-
-def extract_pagination_links(url):
-    html = fetch_html(url)
-    soup = BeautifulSoup(html, 'html.parser')
-    pagination_links = soup.select('nav.paginator > ul > li > a')
-    return pagination_links
-
-
-def extract_item_links(url):
-    html = fetch_html(url)
-    soup = BeautifulSoup(html, 'html.parser')
-    item_links = soup.select('a.js-item-ad')
-
-    filtered_links = []
-    for item_link in item_links:
-        if 'booster' not in item_link['href']:
-            new_link = 'https://999.md' + item_link['href']
-            if new_link not in filtered_links:
-                filtered_links.append(new_link)
-
-    return filtered_links
-
-
-def get_all_item_links(start_url):
-    pagination_links = extract_pagination_links(start_url)
-
-    all_item_links = []
-    for pagination_link in pagination_links:
-        item_links = extract_item_links('https://999.md' + pagination_link['href'])
-        all_item_links.extend(item_links)
-
-    return all_item_links
-
-
-if __name__ == "__main__":
-    all_links = get_all_item_links(BASE_URL)
-    print(all_links)
-    print(len(all_links))
+URL_array = ["https://999.md/ru/list/computers-and-office-equipment/game-consoles"]
+links = recursive(100000000, 0, URL_array, [])
